785bf69d12773bbb3963d9e4466a52f94724fcb4
Divyansh Maheshwari <divyansh2209@gmail.com>
2021-07-25 12:14:35 +0530

    code simplification

--- main.py
+++ main.py
@@ -1,37 +1,26 @@
-import json
 import csv
-from json import JSONEncoder
 from flask import Flask, jsonify
-
 
 app = Flask(__name__)
 
-class User:
-    def __init__(self, uid):
-        with open('user_details.csv') as datafile:
-            entry = csv.reader(datafile)
-            for row in entry:
-                if uid == row[0]:
-                    data = row
-
-        self.name = data[1]
-        self.gamename = data[2]
-        self.elo = data[3]
-        self.played = data[4]
-        self.won = data[5]
-        self.percent = data[6]
-
-
-# subclass JSONEncoder
-class Encoder(JSONEncoder):
-    def default(self, o):
-        return o.__dict__
 
 @app.route("/<string:username>", methods=['GET'])
 def user_details(username: str):
-    user = User(username)
-    formatteddata = json.dumps(user, indent=4, cls=Encoder)
-    return jsonify(formatteddata)
+    with open('user_details.csv') as datafile:
+        entry = csv.reader(datafile)
+        for row in entry:
+            if username == row[0]:
+                data = row
+
+    name = data[1]
+    gamename = data[2]
+    elo = data[3]
+    played = data[4]
+    won = data[5]
+    percent = data[6]
+
+    processed = {'name': name, 'gamename':gamename, 'elo':elo, 'played':played, 'won':won, 'percent':percent}
+    return jsonify(**processed)
 
 
 if __name__ == "__main__":
